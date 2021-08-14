78379987f38670f88e241d2c89f02913aba702bf
John Skelton <johnnyskelton0@gmail.com>
2021-08-15 07:32:08 +1200

    added .travis.yml

---
+++ src/main/java/org/apache/maven/Calc.java
@@ -0,0 +1,15 @@
+package org.apache.maven;
+
+public class Calc
+{
+    public static int add(int a, int b)
+    {
+        int sum = a + b;
+        return sum;
+    }
+    public static int subtract(int a, int b)
+    {
+            int sum = a + b;
+            return sum;
+    }
+}
---
+++ src/test/java/org/apache/maven/TestCalc.java
@@ -0,0 +1,22 @@
+package org.apache.maven;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.apache.maven.Calc;
+/**
+ * Unit test for simple App.
+ */
+public class TestCalc {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    @Test
+    public void TestCalc() {
+
+
+        assertEquals(2,Calc.add(1, 1));
+    }
+}
+
